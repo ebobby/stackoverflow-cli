5dd147073cd498c1af19735df3e8338d796a3222
Francisco Soto <ebobby@gmail.com>
2011-12-18 01:55:59 -0700

    Fixed a bug with the curl requests. Didn't realize the data transfer was done in several calls instead of a single one.

--- requests.c
+++ requests.c
@@ -7,16 +7,16 @@ static size_t request_callback (char *ptr, size_t size, size_t nmemb, void *usr)
     responseObject *response = (responseObject *)usr;
 
     if (realsize != 0) {
-        response->data = (char *)malloc(realsize + 1);
+        response->data = realloc(response->data, response->size + realsize + 1);
 
         if (response->data == NULL) {
             printf("Not enough memory!");
             exit(EXIT_FAILURE);
         }
 
-        memcpy((void *)response->data, (void *)ptr, realsize);
-        response->data[realsize] = 0;
-        response->size = realsize;
+        memcpy(&response->data[response->size], ptr, realsize);
+        response->size += realsize;
+        response->data[response->size] = 0;
     } else {
         response->data = NULL;
         response->size = 0;
@@ -31,6 +31,8 @@ void www_make_request (const char *url, responseObject *response) {
     curl_global_init(CURL_GLOBAL_ALL);
 
     curlHandle = curl_easy_init();
+
+    memset(response, 0, sizeof(responseObject));
 
     curl_easy_setopt(curlHandle, CURLOPT_URL, url);
     curl_easy_setopt(curlHandle, CURLOPT_ACCEPT_ENCODING, "\0");
